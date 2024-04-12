3340d1fa6abf7602632da9736f1ef0aa1fedac9f
Shyam792004 <124516994+Shyam792004@users.noreply.github.com>
2024-04-12 11:23:23 +0530

    Delete backend/heaven directory

--- backend/heaven/src/main/java/suren/heaven/exception/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package suren.heaven.exception;
-
-public class UserNotFoundException extends RuntimeException
-{
-    public UserNotFoundException(Long id)
-    {
-        super("Could not find the user with id"+id);
-    }
-}
--- backend/heaven/src/main/java/suren/heaven/repository/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package suren.heaven.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import suren.heaven.model.User;
-
-public interface UserRepo  extends JpaRepository<User,Long>
-{
-    User findByEmail(String username);
-}
